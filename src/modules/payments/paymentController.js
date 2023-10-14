c0b45bf9c9d80bbe17e5aa93c76e063cde002ed4
SohilaSayed1 <145256809+SohilaSayed1@users.noreply.github.com>
2023-10-14 18:36:07 +0300

    Update paymentController.js

--- src/modules/payments/paymentController.js
+++ src/modules/payments/paymentController.js
@@ -35,7 +35,7 @@ export const confirmedPaymentSchema = Joi.object({
 export const updateRecord = (req,res)=>{
     const error =confirmedPaymentSchema.validate(req.body).error
     if(error){
-        return res.send(error)
+        return res.json({stack : error.stack})
     }else {
         payment.update({PaymentMethodId:req.body.PaymentMethodId,
                         state:'successful'},{where: {id:req.body.id}})
